552728882fabb008abca66dec8cf14b93b7a758a
darcy <darcy3057@gmail.com>
2023-03-06 09:59:48 +1100

    Use `stilo`

--- src/lib.rs
+++ src/lib.rs
@@ -1,133 +1,133 @@
+use stilo::{style, stylize};
+
 pub type GridLine = String;
 pub type Grid = Vec<GridLine>;
 
 /// Game state
 pub enum State {
-  Play,
-  Loss,
-  Win,
+    Play,
+    Loss,
+    Win,
 }
 
 /// Get random item of vector
 pub fn random_item<'a, T>(vec: &'a Vec<T>) -> &'a T {
-  use rand::seq::SliceRandom;
+    use rand::seq::SliceRandom;
 
-  vec
-    .choose(&mut rand::thread_rng())
-    .expect("Could not get random value of vector")
+    vec.choose(&mut rand::thread_rng())
+        .expect("Could not get random value of vector")
 }
 
 /// Generate lines to print
 pub fn get_lines(grid: &mut Grid, answer: &str) -> Vec<String> {
-  let mut print: Vec<String> = vec![];
+    let mut print: Vec<String> = vec![];
 
-  //? Move inside loop ? Might not affect performance - Also change `get_greens`
-  let answer_vec = answer.chars().collect::<Vec<char>>();
+    //? Move inside loop ? Might not affect performance - Also change `get_greens`
+    let answer_vec = answer.chars().collect::<Vec<char>>();
 
-  for line in grid {
-    let mut curr = String::from("\x1b[36m│\x1b[0m");
+    for line in grid {
+        let mut curr = stylize!("│": Cyan);
 
-    for (i, ch) in line.chars().enumerate() {
-      if answer_vec.get(i).unwrap_or(&' ') == &ch {
-        curr += "\x1b[32m"; // Green
-      } else if answer_vec.contains(&ch) {
-        curr += "\x1b[33m"; // Yellow
-      } else {
-        curr += "\x1b[0m"; // White / Default
-      }
+        for (i, ch) in line.chars().enumerate() {
+            let style = if answer_vec.get(i).unwrap_or(&' ') == &ch {
+                style!(Green)
+            } else if answer_vec.contains(&ch) {
+                style!(Yellow)
+            } else {
+                style!()
+            };
 
-      //? Add color reset here ?
-      curr.push(ch);
+            curr += &style.format(&ch.to_string());
+        }
+
+        curr += &stylize!("│": Cyan);
+        print.push(curr);
     }
 
-    curr += "\x1b[36m│\x1b[0m";
-    print.push(curr);
-  }
-
-  print
+    print
 }
 
 /// Get user input as String
 pub fn input(prompt: &str) -> Result<String, std::io::Error> {
-  use std::io::{stdin, stdout, Write};
+    use std::io::{stdin, stdout, Write};
 
-  let mut s = String::new();
-  print!("{}", prompt);
+    let mut s = String::new();
+    print!("{}", prompt);
 
-  let _ = stdout().flush();
-  stdin().read_line(&mut s)?;
+    let _ = stdout().flush();
+    stdin().read_line(&mut s)?;
 
-  if let Some('\n') = s.chars().next_back() {
-    s.pop();
-  }
-  if let Some('\r') = s.chars().next_back() {
-    s.pop();
-  }
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
 
-  Ok(s)
+    Ok(s)
 }
 
 /// Remove first character of string
 pub fn remove_first(s: &str) -> &str {
-  let mut chars = s.chars();
-  chars.next();
-  chars.as_str()
+    let mut chars = s.chars();
+    chars.next();
+    chars.as_str()
 }
 
 /// Algorithm to get valid guesses based on grid state
 /// Returns list of all valid words, from list of total answers
 pub fn smart_guess<'a>(grid: &Grid, answer: &str, answers: &Vec<&'a str>) -> Vec<&'a str> {
-  let mut valids = Vec::<&str>::new();
+    let mut valids = Vec::<&str>::new();
 
-  'Guess: for &guess in answers {
-    for row in grid {
-      // Guess must not be in grid already
-      if guess == row {
-        continue 'Guess;
-      }
+    'Guess: for &guess in answers {
+        for row in grid {
+            // Guess must not be in grid already
+            if guess == row {
+                continue 'Guess;
+            }
 
-      // Loop characters
-      for (i, row_ch) in row.chars().enumerate() {
-        let answer_ch = answer
-          .chars()
-          .nth(i)
-          .expect("Row and answer should be same length");
-        let guess_ch = guess
-          .chars()
-          .nth(i)
-          .expect("Row and guess should be same length");
+            // Loop characters
+            for (i, row_ch) in row.chars().enumerate() {
+                let answer_ch = answer
+                    .chars()
+                    .nth(i)
+                    .expect("Row and answer should be same length");
+                let guess_ch = guess
+                    .chars()
+                    .nth(i)
+                    .expect("Row and guess should be same length");
 
-        // Green
-        if answer_ch == row_ch {
-          // Green must be same character
-          if guess_ch != row_ch {
-            continue 'Guess;
-          }
+                // Green
+                if answer_ch == row_ch {
+                    // Green must be same character
+                    if guess_ch != row_ch {
+                        continue 'Guess;
+                    }
+                }
+                // Yellow
+                else if answer.contains(row_ch) {
+                    // Yellow must not be same character
+                    if guess_ch == row_ch {
+                        continue 'Guess;
+                    }
+                    // Yellow must be in word
+                    if !guess.contains(row_ch) {
+                        continue 'Guess;
+                    }
+                }
+                // White
+                else {
+                    // White must not be in word
+                    if guess.contains(row_ch) {
+                        continue 'Guess;
+                    }
+                }
+            }
         }
-        // Yellow
-        else if answer.contains(row_ch) {
-          // Yellow must not be same character
-          if guess_ch == row_ch {
-            continue 'Guess;
-          }
-          // Yellow must be in word
-          if !guess.contains(row_ch) {
-            continue 'Guess;
-          }
-        }
-        // White
-        else {
-          // White must not be in word
-          if guess.contains(row_ch) {
-            continue 'Guess;
-          }
-        }
-      }
+
+        // Add to valid guesses
+        valids.push(guess);
     }
 
-    // Add to valid guesses
-    valids.push(guess);
-  }
-
-  valids
+    valids
 }
--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,175 @@
+use stilo::{print_styles, stylize, stylize_many};
 use vorto::{Grid, State::*};
 
 //TODO Add timing ¿ how ?
 fn main() -> ! {
-  // Handle CTRLC exit
-  ctrlc::set_handler(|| {
-    println!("\nĜis!");
-    std::process::exit(0);
-  })
-  .expect("Could not set CTRLC handler");
+    // Handle CTRLC exit
+    ctrlc::set_handler(|| {
+        println!("\nĜis!");
+        std::process::exit(0);
+    })
+    .expect("Could not set CTRLC handler");
 
-  // Include text files in binary
-  let answers = include_str!("answers.txt").lines().collect::<Vec<&str>>();
-  let accepted = include_str!("accepted.txt").lines().collect::<Vec<&str>>();
-  let mut warning = String::from("");
+    // Include text files in binary
+    let answers = include_str!("answers.txt").lines().collect::<Vec<&str>>();
+    let accepted = include_str!("accepted.txt").lines().collect::<Vec<&str>>();
+    let mut warning = String::from("");
 
-  // Game
-  loop {
-    let answer = vorto::random_item(&answers);
-    let mut grid: Grid = Vec::new();
-    let mut state = Play;
-
-    // Guess
+    // Game
     loop {
-      // Title
-      print!("\x1bc\x1b[36;3m┌VORTO┐\x1b[0m");
-      // print!("\x1b[36;3m┌VORTO┐\x1b[0m [{answer}] "); // Debug title
+        let answer = vorto::random_item(&answers);
+        let mut grid: Grid = Vec::new();
+        let mut state = Play;
 
-      // Warning
-      if warning.len() > 0 {
-        print!(" \x1b[33m{warning}");
-      }
-      if grid.len() > 0 {
-        println!();
-      }
+        // Guess
+        loop {
+            // Clear terminal
+            print!("\x1bc");
+            // Title
+            print_styles!("┌VORTO┐": Cyan + italic);
 
-      // Generate lines
-      let print = vorto::get_lines(&mut grid, &answer);
-      println!("{}", print.join("\n"));
-
-      // Win or loss display
-      if let Win | Loss = state {
-        vorto::input(if let Win = state {
-          "\x1b[36m└\x1b[32;1mVENKO\x1b[0m\x1b[36m┘\x1b[0m"
-        } else {
-          "\x1b[36m└\x1b[31;1mPERDO\x1b[0m\x1b[36m┘\x1b[0m"
-        })
-        .expect("Could not continue from input");
-
-        warning = "".to_string();
-        break; // Break guess loop, start new game
-      }
-
-      // Win check
-      if Some(&answer.to_string()) == grid.last() {
-        state = Win;
-        warning = format!(
-          "\x1b[32m{}",
-          [
-            "GENIULO!",
-            "Lerta!",
-            "Bonega!",
-            "Bona",
-            "Boneta",
-            "Ne malbona...",
-          ]
-          .get(grid.len() - 1)
-          .unwrap_or(&"Kio?")
-        );
-        continue; // Skip rest of guess loop
-      }
-
-      // Loss check
-      if grid.len() >= 6 {
-        state = Loss;
-        warning = format!("Estis: \x1b[3m'{}'", answer);
-        continue; // Skip rest of guess loop
-      }
-
-      // Make guess
-      let guess = vorto::input("\x1b[36m└\x1b[0m")
-        .expect("Could not read standard input")
-        .to_lowercase();
-
-      // Command starts with '/'
-      if guess.starts_with('/') {
-        match vorto::remove_first(&guess) {
-          // New game (restart)
-          "eliru" => {
-            warning = format!("Estis: \x1b[3m'{}'", answer);
-            break; // Break guess loop, start new game
-          }
-
-          // Give answer (cheat)
-          "trompu" => {
-            warning = format!("\x1b[31mEstas: \x1b[3m'{}'", answer);
-          }
-
-          // Random word (guess)
-          "divenu" => {
-            grid.push(vorto::random_item(&answers).to_string());
-            warning = "Hazarda tre".to_string();
-          }
-
-          // Reasonable guess (think)
-          "pensu" => {
-            if grid.len() < 1 {
-              grid.push("salto".to_string());
-              warning = "Boneta diveno".to_string();
-            } else {
-              let valids = vorto::smart_guess(&grid, answer, &answers);
-              if valids.len() > 0 {
-                grid.push(vorto::random_item(&valids).to_string());
-                warning = format!("Eblaj vortoj: {}", valids.len());
-              } else {
-                warning = "Ne povas trovi la solvon!".to_string();
-              }
+            // Warning
+            if warning.len() > 0 {
+                print_styles!(" {warning}": Yellow);
             }
-          }
-
-          // Remove last guess (fix)
-          "riparu" => {
             if grid.len() > 0 {
-              grid.pop().expect("Could not remove last item of grid");
-              warning = "Uŭps!".to_string();
-            } else {
-              warning = "Ne povas ripari".to_string();
+                println!();
             }
-          }
 
-          // Unknown command
-          _ => {
-            warning = "Nekonata ordono".to_string();
-          }
+            // Generate lines
+            let print = vorto::get_lines(&mut grid, &answer);
+            println!("{}", print.join("\n"));
+
+            // Win or loss display
+            if let Win | Loss = state {
+                vorto::input(&if let Win = state {
+                    stylize_many!(
+                        "└": Cyan;
+                        "VENKO": Green + italic;
+                        "┘": Cyan;
+                    )
+                } else {
+                    stylize_many!(
+                        "└": Cyan;
+                        "PERDO": Red + italic;
+                        "┘": Cyan;
+                    )
+                })
+                .expect("Could not continue from input");
+
+                warning = "".to_string();
+                break; // Break guess loop, start new game
+            }
+
+            // Win check
+            if Some(&answer.to_string()) == grid.last() {
+                state = Win;
+                warning = stylize!(
+                    "{}": Green,
+                    [
+                        "GENIULO!",
+                        "Lerta!",
+                        "Bonega!",
+                        "Bona",
+                        "Boneta",
+                        "Ne malbona...",
+                    ]
+                    .get(grid.len() - 1)
+                    .unwrap_or(&"Kio?")
+                );
+                continue; // Skip rest of guess loop
+            }
+
+            // Loss check
+            if grid.len() >= 6 {
+                state = Loss;
+                warning = stylize_many!(
+                    "Estis: ";
+                    "'{}'": +italic, answer;
+                );
+                continue; // Skip rest of guess loop
+            }
+
+            // Make guess
+            let guess = vorto::input(&stylize!("└": Cyan))
+                .expect("Could not read standard input")
+                .to_lowercase();
+
+            // Command starts with '/'
+            if guess.starts_with('/') {
+                match vorto::remove_first(&guess) {
+                    // New game (restart)
+                    "eliru" => {
+                        warning = stylize_many!(
+                            "Estis: ";
+                            "'{}'": +italic, answer;
+                        );
+                        break; // Break guess loop, start new game
+                    }
+
+                    // Give answer (cheat)
+                    "trompu" => {
+                        warning = stylize_many!(
+                            "Estas: ";
+                            "'{}'": +italic, answer;
+                        );
+                    }
+
+                    // Random word (guess)
+                    "divenu" => {
+                        grid.push(vorto::random_item(&answers).to_string());
+                        warning = "Hazarda tre".to_string();
+                    }
+
+                    // Reasonable guess (think)
+                    "pensu" => {
+                        if grid.len() < 1 {
+                            grid.push("salto".to_string());
+                            warning = "Boneta diveno".to_string();
+                        } else {
+                            let valids = vorto::smart_guess(&grid, answer, &answers);
+                            if valids.len() > 0 {
+                                grid.push(vorto::random_item(&valids).to_string());
+                                warning = format!("Eblaj vortoj: {}", valids.len());
+                            } else {
+                                warning = "Ne povas trovi la solvon!".to_string();
+                            }
+                        }
+                    }
+
+                    // Remove last guess (fix)
+                    "riparu" => {
+                        if grid.len() > 0 {
+                            grid.pop().expect("Could not remove last item of grid");
+                            warning = "Uŭps!".to_string();
+                        } else {
+                            warning = "Ne povas ripari".to_string();
+                        }
+                    }
+
+                    // Unknown command
+                    _ => {
+                        warning = "Nekonata ordono".to_string();
+                    }
+                }
+
+                continue; // Skip rest of guess loop
+            }
+
+            // Guess must be 5 letters
+            if guess.chars().count() != 5 {
+                warning = "Devas esti 5 literoj".to_string();
+                continue; // Skip rest of guess loop
+            }
+            // Must be in either word list
+            if !(answers.contains(&guess.as_str()) || accepted.contains(&guess.as_str())) {
+                warning = "Ne estas vorto".to_string();
+                continue; // Skip rest of guess loop
+            }
+
+            // Guess accepted
+            grid.push(guess);
+            warning = "".to_string();
         }
-
-        continue; // Skip rest of guess loop
-      }
-
-      // Guess must be 5 letters
-      if guess.chars().count() != 5 {
-        warning = "Devas esti 5 literoj".to_string();
-        continue; // Skip rest of guess loop
-      }
-      // Must be in either word list
-      if !(answers.contains(&guess.as_str()) || accepted.contains(&guess.as_str())) {
-        warning = "Ne estas vorto".to_string();
-        continue; // Skip rest of guess loop
-      }
-
-      // Guess accepted
-      grid.push(guess);
-      warning = "".to_string();
     }
-  }
 }
